f134817d5a9ac4f4246af20f50ffd6a381d4eb9b
Joserafpg <109305829+Joserafpg@users.noreply.github.com>
2026-08-09 21:25:20 -0400

    crear entidad proveedor

---
+++ CapaEntidades/Proveedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Proveedor
+    {
+        public int IdProveedor { get; set; }
+        public string Nombre { get; set; }
+        public string Rnc { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Direccion { get; set; }
+        public bool Estado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
