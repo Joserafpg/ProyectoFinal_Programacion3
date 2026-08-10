432ced15eca2b85b86fae2cbd88255026f46d81a
Joserafpg <109305829+Joserafpg@users.noreply.github.com>
2026-08-10 19:46:51 -0400

    Consultar horarios

--- ProyectoFinal_Programacion3/FrmHorarios.cs
+++ ProyectoFinal_Programacion3/FrmHorarios.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Windows.Forms;
+using CapaNegocio;
 
 namespace ProyectoFinal_Programacion3
 {
     public partial class FrmHorarios : Form
     {
+        HorarioNegocio horarioNegocio = new HorarioNegocio();
         public FrmHorarios()
         {
             InitializeComponent();
+        }
+
+        private void FrmHorarios_Load(object sender, EventArgs e)
+        {
+            dgvDatos.DataSource = horarioNegocio.Listar();
         }
     }
 }
