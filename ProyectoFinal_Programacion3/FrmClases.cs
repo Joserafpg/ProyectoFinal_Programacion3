a773c0f01d474da5c78ceebe02c2695a56e9e786
Joserafpg <109305829+Joserafpg@users.noreply.github.com>
2026-08-10 19:49:00 -0400

    Consultar clases

--- ProyectoFinal_Programacion3/FrmClases.cs
+++ ProyectoFinal_Programacion3/FrmClases.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Windows.Forms;
+using CapaNegocio;
 
 namespace ProyectoFinal_Programacion3
 {
     public partial class FrmClases : Form
     {
+        ClaseNegocio claseNegocio = new ClaseNegocio();
         public FrmClases()
         {
             InitializeComponent();
+        }
+
+        private void FrmClases_Load(object sender, EventArgs e)
+        {
+            dgvDatos.DataSource = claseNegocio.Listar();
         }
     }
 }
