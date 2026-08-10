dcdabb056b1c4b56a249f49473210d8908d150f3
Joserafpg <109305829+Joserafpg@users.noreply.github.com>
2026-08-09 20:57:46 -0400

    Creacion Entidades Permisos y Rol

---
+++ CapaEntidades/Permisos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Permisos
+    {
+        public int IdPermiso { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
