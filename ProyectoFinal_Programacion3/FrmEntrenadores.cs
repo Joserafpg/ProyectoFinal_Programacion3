e0029c01b55ad4e9f68c542f43a0e985df265243
Joserafpg <109305829+Joserafpg@users.noreply.github.com>
2026-08-10 19:45:38 -0400

    Consultar entrenadores

--- ProyectoFinal_Programacion3/FrmEntrenadores.cs
+++ ProyectoFinal_Programacion3/FrmEntrenadores.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Windows.Forms;
+using CapaNegocio;
 
 namespace ProyectoFinal_Programacion3
 {
     public partial class FrmEntrenadores : Form
     {
+        EntrenadorNegocio entrenadorNegocio = new EntrenadorNegocio();
         public FrmEntrenadores()
         {
             InitializeComponent();
+        }
+
+        private void FrmEntrenadores_Load(object sender, EventArgs e)
+        {
+            dgvDatos.DataSource = entrenadorNegocio.Listar();
         }
     }
 }
