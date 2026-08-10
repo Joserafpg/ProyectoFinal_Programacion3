e698bff5588825185005bd313fdebd5780ccdecf
Joserafpg <109305829+Joserafpg@users.noreply.github.com>
2026-08-10 19:53:32 -0400

    Consulta usuarios

--- ProyectoFinal_Programacion3/FrmUsuarios.cs
+++ ProyectoFinal_Programacion3/FrmUsuarios.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Windows.Forms;
+using CapaNegocio;
 
 namespace ProyectoFinal_Programacion3
 {
     public partial class FrmUsuarios : Form
     {
+        UsuarioNegocio usuarioNegocio = new UsuarioNegocio();
+
         public FrmUsuarios()
         {
             InitializeComponent();
+        }
+
+        private void FrmUsuarios_Load(object sender, EventArgs e)
+        {
+            dgvDatos.DataSource = usuarioNegocio.Listar();
         }
     }
 }
