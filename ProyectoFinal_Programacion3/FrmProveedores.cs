3b24a6c115a9af52a0f2ab6062fbae04046d2497
Joserafpg <109305829+Joserafpg@users.noreply.github.com>
2026-08-10 19:43:19 -0400

    Consultar proveedores

--- ProyectoFinal_Programacion3/FrmProveedores.cs
+++ ProyectoFinal_Programacion3/FrmProveedores.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Windows.Forms;
+using CapaNegocio;
 
 namespace ProyectoFinal_Programacion3
 {
     public partial class FrmProveedores : Form
     {
+        ProveedorNegocio proveedorNegocio = new ProveedorNegocio();
         public FrmProveedores()
         {
             InitializeComponent();
+        }
+
+        private void FrmProveedores_Load(object sender, EventArgs e)
+        {
+            dgvDatos.DataSource = proveedorNegocio.Listar();
         }
     }
 }
