1ac00572401beeca56f0119f5113035605995560
Joserafpg <109305829+Joserafpg@users.noreply.github.com>
2026-08-10 19:41:02 -0400

    Consultar Marcas

--- ProyectoFinal_Programacion3/FrmMarcas.Designer.cs
+++ ProyectoFinal_Programacion3/FrmMarcas.Designer.cs
@@ -65,20 +65,25 @@
             this.dgvDatos.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvDatos.EnableHeadersVisualStyles = false;
             this.dgvDatos.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(237)))), ((int)(((byte)(243)))));
+            this.dgvDatos.Location = new System.Drawing.Point(37, 106);
+            this.dgvDatos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvDatos.MultiSelect = false;
             this.dgvDatos.Name = "dgvDatos";
             this.dgvDatos.ReadOnly = true;
             this.dgvDatos.RowHeadersVisible = false;
+            this.dgvDatos.RowHeadersWidth = 51;
             this.dgvDatos.RowTemplate.Height = 38;
             this.dgvDatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvDatos.Size = new System.Drawing.Size(988, 542);
+            this.dgvDatos.Size = new System.Drawing.Size(1318, 667);
             this.dgvDatos.TabIndex = 0;
             // 
             // spGrid
             // 
             this.spGrid.Dock = System.Windows.Forms.DockStyle.Top;
+            this.spGrid.Location = new System.Drawing.Point(37, 84);
+            this.spGrid.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.spGrid.Name = "spGrid";
-            this.spGrid.Size = new System.Drawing.Size(988, 18);
+            this.spGrid.Size = new System.Drawing.Size(1318, 22);
             this.spGrid.TabIndex = 0;
             // 
             // panelBarra
@@ -87,8 +92,10 @@
             this.panelBarra.Controls.Add(this.spNuevo);
             this.panelBarra.Controls.Add(this.btnNuevo);
             this.panelBarra.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelBarra.Location = new System.Drawing.Point(37, 30);
+            this.panelBarra.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelBarra.Name = "panelBarra";
-            this.panelBarra.Size = new System.Drawing.Size(988, 44);
+            this.panelBarra.Size = new System.Drawing.Size(1318, 54);
             this.panelBarra.TabIndex = 1;
             // 
             // panelBusqueda
@@ -97,9 +104,11 @@
             this.panelBusqueda.Controls.Add(this.txtBuscar);
             this.panelBusqueda.Controls.Add(this.lblLupa);
             this.panelBusqueda.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBusqueda.Location = new System.Drawing.Point(0, 0);
+            this.panelBusqueda.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panelBusqueda.Name = "panelBusqueda";
-            this.panelBusqueda.Padding = new System.Windows.Forms.Padding(14, 12, 14, 12);
-            this.panelBusqueda.Size = new System.Drawing.Size(842, 44);
+            this.panelBusqueda.Padding = new System.Windows.Forms.Padding(19, 15, 19, 15);
+            this.panelBusqueda.Size = new System.Drawing.Size(1124, 54);
             this.panelBusqueda.TabIndex = 0;
             // 
             // txtBuscar
@@ -109,8 +118,10 @@
             this.txtBuscar.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtBuscar.Font = new System.Drawing.Font("Segoe UI", 10.5F);
             this.txtBuscar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(75)))), ((int)(((byte)(100)))));
+            this.txtBuscar.Location = new System.Drawing.Point(59, 15);
+            this.txtBuscar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtBuscar.Name = "txtBuscar";
-            this.txtBuscar.Size = new System.Drawing.Size(784, 20);
+            this.txtBuscar.Size = new System.Drawing.Size(1046, 24);
             this.txtBuscar.TabIndex = 1;
             // 
             // lblLupa
@@ -118,16 +129,20 @@
             this.lblLupa.Dock = System.Windows.Forms.DockStyle.Left;
             this.lblLupa.Image = global::ProyectoFinal_Programacion3.Properties.Resources.icono_buscar;
             this.lblLupa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblLupa.Location = new System.Drawing.Point(19, 15);
+            this.lblLupa.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblLupa.Name = "lblLupa";
-            this.lblLupa.Size = new System.Drawing.Size(30, 20);
+            this.lblLupa.Size = new System.Drawing.Size(40, 24);
             this.lblLupa.TabIndex = 0;
             this.lblLupa.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // spNuevo
             // 
             this.spNuevo.Dock = System.Windows.Forms.DockStyle.Right;
+            this.spNuevo.Location = new System.Drawing.Point(1124, 0);
+            this.spNuevo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.spNuevo.Name = "spNuevo";
-            this.spNuevo.Size = new System.Drawing.Size(16, 44);
+            this.spNuevo.Size = new System.Drawing.Size(21, 54);
             this.spNuevo.TabIndex = 0;
             // 
             // btnNuevo
@@ -140,25 +155,29 @@
             this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnNuevo.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
             this.btnNuevo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(63)))), ((int)(((byte)(22)))));
+            this.btnNuevo.Location = new System.Drawing.Point(1145, 0);
+            this.btnNuevo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnNuevo.Name = "btnNuevo";
-            this.btnNuevo.Size = new System.Drawing.Size(130, 44);
+            this.btnNuevo.Size = new System.Drawing.Size(173, 54);
             this.btnNuevo.TabIndex = 2;
             this.btnNuevo.Text = "+  Nuevo";
             this.btnNuevo.UseVisualStyleBackColor = false;
             // 
             // FrmMarcas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(251)))), ((int)(((byte)(253)))));
-            this.ClientSize = new System.Drawing.Size(1040, 656);
+            this.ClientSize = new System.Drawing.Size(1387, 807);
             this.Controls.Add(this.dgvDatos);
             this.Controls.Add(this.spGrid);
             this.Controls.Add(this.panelBarra);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmMarcas";
-            this.Padding = new System.Windows.Forms.Padding(28, 24, 24, 28);
+            this.Padding = new System.Windows.Forms.Padding(37, 30, 32, 34);
             this.Text = "Marcas";
+            this.Load += new System.EventHandler(this.FrmMarcas_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
             this.panelBarra.ResumeLayout(false);
             this.panelBusqueda.ResumeLayout(false);
--- ProyectoFinal_Programacion3/FrmMarcas.cs
+++ ProyectoFinal_Programacion3/FrmMarcas.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Windows.Forms;
+using CapaNegocio;
 
 namespace ProyectoFinal_Programacion3
 {
     public partial class FrmMarcas : Form
     {
+        MarcaNegocio marcaNegocio = new MarcaNegocio();
         public FrmMarcas()
         {
             InitializeComponent();
+        }
+
+        private void FrmMarcas_Load(object sender, EventArgs e)
+        {
+            dgvDatos.DataSource = marcaNegocio.Listar();
         }
     }
 }
