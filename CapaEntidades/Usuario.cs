ceecdb183d8f12a543f3f1fdf8871e79f6acb8db
Joserafpg <109305829+Joserafpg@users.noreply.github.com>
2026-08-09 21:02:22 -0400

    Crear entidad USUARIOS

---
+++ CapaEntidades/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Clave { get; set; }
+        public string Correo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public bool Estado { get; set; }
+
+        public int IdRol { get; set; }
+        public string NombreRol { get; set; }
+        public List<string> Permisos { get; set; }
+    }
+}
